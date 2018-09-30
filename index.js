3312a84caebab477f00aae90f020c0103846d14f
Daniel Kang <danielkang674@gmail.com>
2018-09-29 20:07:17 -0500

    Port env

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.end('hi');
+  res.send('hi');
 });
 
 app.listen(PORT, () => console.log('started'));
