4911a3fe107a94d43c1d6a2dd5c8d00473ce4947
WalkerJG33 <38767282+WalkerJG33@users.noreply.github.com>
2018-10-03 11:59:10 -0500

    Reccomendation routes added (#12)

--- api/movies.js
+++ api/movies.js
@@ -1,17 +1,31 @@
 const { get }  = require('axios');
 const api_key = process.env.MOVIE_API_KEY;
-const url = 'https://api.themoviedb.org/3/search/movie'
 
 module.exports.getMovieByTitle = (req, res) => {
+  const url = 'https://api.themoviedb.org/3/search/movie';
   const { query } = req.params;
   const params = {
     query,
     api_key
-  }
+  };
 
   get(url, { params })
   .then(response => {
     res.send(response.data.results);
   })
-  .catch(console.log)
-} 
+  .catch(console.log);
+}
+
+module.exports.getMovieRecc = (req, res) => {
+  const movie_id = req.params.movieId;
+  const url = `https://api.themoviedb.org/3/movie/${movie_id}/recommendations`;
+  const params = {
+    api_key
+  };
+
+  get(url, { params })
+  .then(response => {
+    res.send(response.data.results);
+  })
+  .catch(console.log);
+}
