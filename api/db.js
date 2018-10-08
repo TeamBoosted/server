3602243ccc0b53091847e90e46a952490d5ba8f1
WalkerJG33 <wgriffinj@gmail.com>
2018-10-08 11:54:47 -0500

    connected routes to render returning users

--- api/db.js
+++ api/db.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
+const { formatUrl } = require('../helpers/helper.js');
 
 module.exports.saveMediumToDb = (req, res) => {
-  let mediumObj = req.body.data.movie;
-  let id_token = req.body.data.user;
+  const mediumObj = req.body.data.movie;
+  const id_token = req.body.data.user;
   axios.post('http://localhost:8081/db/addMedium', { mediumObj, id_token })
     .then(() => {
       res.sendStatus(200);
@@ -14,14 +15,19 @@ module.exports.saveMediumToDb = (req, res) => {
 };
 
 module.exports.getLastThreeMedia = (req, res) => {
-  let id_token = req.body.data.user;
-  console.log(id_token);
-  axios.post('http://localhost:8081/db/getLastThreeMedia', { id_token })
-    .then(data => {
-      res.json(data.data);
-    })
-    .catch(err => {
-      console.log(err);
-      res.sendStatus(500);
-    });
+  const id_token = req.body.data.id_token;
+  axios
+    .post('http://localhost:8081/db/getLastThreeMedia', { id_token })
+      .then(data => {
+        const url = formatUrl(data.data);
+        axios
+          .get(url)
+            .then(response => {
+              res.send(response.data);
+            })
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+      });
 };
