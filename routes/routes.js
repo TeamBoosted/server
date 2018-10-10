95eadabe290887179a6438de5034da01ae533131
WalkerJG33 <wgriffinj@gmail.com>
2018-10-10 16:14:43 -0500

    fixing for PR

--- routes/routes.js
+++ routes/routes.js
@@ -4,9 +4,6 @@ const { getByTvTitle, getTvRecc } = require('../api/tv.js');
 const { getMovieByTitle, getMovieRecc, getManyMovieReccs } = require('../api/movies.js');
 const { saveMediumToDb, getLastThreeMedia } = require('../api/db.js');
 
-
-client.on('error', console.log);
-
 //TV Routes
 router
   .route('/info/tv/:query')
