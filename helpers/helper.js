1a6b44f07ee3e992bebcbde65943553421998737
Daniel Kang <40406108+danielkang674@users.noreply.github.com>
2018-10-11 16:44:13 -0500

    Refactor/devops prune (#34)
    * Readme
    
    * Devops circleci and rebuild prune

--- helpers/helper.js
+++ helpers/helper.js
@@ -13,14 +13,14 @@ module.exports.formatData = (res, type) => {
     })
   })
   return formatted;
-} 
+}
 
 module.exports.limitToFive = (array) => {
   const limitted = [];
   if (array.length < 5) {
     return [...array];
-  } 
-  for(let i = 0; i < 5; i++) {
+  }
+  for (let i = 0; i < 5; i++) {
     limitted.push(array[i]);
   }
   return limitted;
--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,6 @@ if [ "$1" == "nocontainer" ]; then
   echo "Starting new container.."
 else
   echo "Restarting container.."
-  docker stop teamboosted-server && docker rm -f -v teamboosted-server
+  docker stop teamboosted-server && docker rm -f -v teamboosted-server && docker image prune -af && docker system prune -af
 fi
 docker build -t teamboosted-server . && docker run --volumes-from teamboosted-client --name teamboosted-server -p 80:80 --network br0 --env-file .env -d teamboosted-server
