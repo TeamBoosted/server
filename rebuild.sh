741e99ecbd6187eb2aca7a11fbcb3643f5ea5e21
Daniel Kang <danielkang674@gmail.com>
2018-10-12 17:33:57 -0500

    not username for stop

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,6 @@ if [ "$1" == "nocontainer" ]; then
   echo "Starting new container.."
 else
   echo "Restarting container.."
-  docker stop danielkang674/teamboosted-server:latest && docker rm -f -v danielkang674/teamboosted-server:latest && docker image prune -af && docker system prune -af
+  docker stop teamboosted-server && docker rm -f teamboosted-server && docker image prune -af && docker system prune -af
 fi
 docker pull danielkang674/teamboosted-server:latest && docker run --volumes-from teamboosted-client --name teamboosted-server -p 80:80 --network br0 --env-file .env -d danielkang674/teamboosted-server:latest
