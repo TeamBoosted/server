7c189cd8e7abe62172c5fce63ac06f18556a7ff0
Walker Griffin <38767282+WalkerJG33@users.noreply.github.com>
2018-10-16 15:50:08 -0500

    Filtering complete (#47)

--- api/tv.js
+++ api/tv.js
@@ -54,7 +54,6 @@ module.exports.tvRecByGenre = async (req, res) => {
  
   try {
     const response = await axios.get(url, { params })
-    console.log('----------\nGetting response from discover\n----------\n', response);
     const formatted = formatData(response.data.results, 'tv');
     res.send(formatted);
   } catch (err) {
